56dd0eed8a98e22fea515c4f8cae4176da4818dd
Ainun Najib <ainunnajib@gmail.com>
2022-09-11 18:17:02 +0800

    trivial

---
+++ psle.py
@@ -0,0 +1,15 @@
+n = int(input())
+if n < 20:
+	print('U')
+elif n < 35:
+	print('E')
+elif n < 50:
+	print('D')
+elif n < 60:
+	print('C')
+elif n < 75:
+	print('B')
+elif n < 91:
+	print('A')
+else:
+	print('A*')
