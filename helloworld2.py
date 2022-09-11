5190a22a6c1af70f4d50d1c63a7abd3a03539a8b
Ainun Najib <ainunnajib@gmail.com>
2022-09-11 18:03:07 +0800

    joke problem, use inspect & add the submit button

---
+++ helloworld2.py
@@ -0,0 +1,3 @@
+# add the HTML code for submit button via inspect tool
+#<p id="submitButtonParagraph">  <input class="btn btn-primary" id="submit" name="submit" onclick="onSubmit()" type="submit" value="Submit Code"> </p>
+print('Hello World')
